d146fdfcb245d8e469e9b90d77d8af719f334ae7
Jorge <xmiven@gmail.com>
2022-10-13 11:50:25 +0200

    added file option

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const addPicture = function () {
 	let image = document.createElement('img');
 	let url = document.getElementById('url').value;
 	let alt = document.getElementById('alt').value;
+	let file = document.getElementById('file').value;
 	image.setAttribute('class', 'images');
-	image.setAttribute('src', url);
+	if (file !== '') {
+		image.setAttribute('src', file);
+	} else {
+		image.setAttribute('src', url);
+	}
 	image.setAttribute('alt', alt);
 	//content.appendChild(image);//
 	content.insertBefore(image, content.children[0]);
